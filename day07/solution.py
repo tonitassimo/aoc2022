c21ab1063271e0a5fc434289d748a0a3c32e5a8b
antonio <tonitassimo@gmail.com>
2023-09-04 09:36:46 +0200

    Add skeleton in solution.py

--- day07/solution.py
+++ day07/solution.py
@@ -1,3 +1,28 @@
 def solve():
     puzzle_input = open("day07/data.txt", "r").readlines()
-    print(puzzle_input[:1])
+    for line in puzzle_input:
+        process_line(line)
+
+
+def process_line(line: str):
+    if line.startswith("$"):
+        handle_command(line[2:])
+    else:
+        handle_result(line)
+
+
+def handle_command(command: str):
+    if command.startswith("cd"):
+        handle_cd(command[4:])
+    elif command.startswith("ls"):
+        handle_ls()
+
+
+def handle_cd(directory: str):
+    None
+
+def handle_ls():
+    None
+
+def handle_result(result: str):
+    print(result)
