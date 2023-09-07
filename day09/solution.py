c5b680be8cff5919800634a802d06c7b5e50b679
antonio <tonitassimo@gmail.com>
2023-09-07 12:56:38 +0200

    Add skeleton and test data for Day 9

---
+++ day09/solution.py
@@ -0,0 +1,4 @@
+def solve():
+    puzzle = open("day09/test.txt", "r").readlines()
+    for line in puzzle:
+        print(line[:-1])
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # import day07.solution as day07_solution
-import day08.solution as day08_solution
+# import day08.solution as day08_solution
+import day09.solution as day09_solution
 
 if __name__ == '__main__':
     # day07_solution.solve()
-    day08_solution.solve()
+    # day08_solution.solve()
+    day09_solution.solve()
