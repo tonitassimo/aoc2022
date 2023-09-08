67c5a35b460f63e5f094e739d021c5320071382a
antonio <tonitassimo@gmail.com>
2023-09-08 17:12:48 +0200

    Solve Part One of Day 25

--- day25/converter.py
+++ day25/converter.py
@@ -1,9 +1,52 @@
+base = 5
+
+
+# DECIMAL TO SNAFU:
 def decimal_to_snafu(decimal):
-    pass
+    base_5 = decimal_to_base_5(decimal)
+    snafu_digits = base_5_to_snafu_digits(base_5)
+    snafu_string = digits_to_snafu(snafu_digits)
+    return snafu_string
 
 
+def decimal_to_base_5(decimal):
+    result = []
+    while decimal != 0:
+        rest = decimal % base
+        decimal = decimal // base
+        result.append(rest)
+    return result
+
+
+def base_5_to_snafu_digits(base_5):
+    result = []
+    for index, digit in enumerate(base_5):
+        if digit > 2:
+            result.append(digit - base)
+            if index < len(base_5) - 1:
+                base_5[index + 1] += 1
+            else:
+                result.append(1)
+        else:
+            result.append(digit)
+    result.reverse()
+    return result
+
+
+def digits_to_snafu(digits):
+    return [digit_to_snafu(digit) for digit in digits]
+
+
+def digit_to_snafu(digit):
+    if digit == -1:
+        return '-'
+    if digit == -2:
+        return '='
+    return str(digit)
+
+
+# SNAFU TO DECIMAL:
 def snafu_to_decimal(snafu):
-    base = 5
     snafu = parse_snafu(snafu)
     length = len(snafu)
     result = 0
--- day25/solution.py
+++ day25/solution.py
@@ -1,7 +1,15 @@
-from day25.converter import snafu_to_decimal
+from day25.converter import snafu_to_decimal, decimal_to_snafu
+
 
 def solve():
-    puzzle = open("day25/test.txt", "r").readlines()
+    puzzle = open("day25/data.txt", "r").readlines()
+    result = 0
     for line in puzzle:
         line = line[:-1]
-        print(line + " -> " + str(snafu_to_decimal(line)))
+        decimal = snafu_to_decimal(line)
+        result += decimal
+        # snafu = decimal_to_snafu(decimal)
+        # print(line + " --> " + str(decimal) + " --> " + str("".join(snafu)))
+    snafu = decimal_to_snafu(result)
+    print(result)
+    print("".join(snafu))
