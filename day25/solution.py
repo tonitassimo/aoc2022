0b0b0ee28905e98b30cadfaa575dbd09400538b5
antonio <tonitassimo@gmail.com>
2023-09-08 19:59:44 +0200

    Improve logging

--- day25/solution.py
+++ day25/solution.py
@@ -1,15 +1,17 @@
-from day25.converter import snafu_to_decimal, decimal_to_snafu
+from day25.converter import snafu_to_decimal, decimal_to_snafu, decimal_to_base_5
 
 
 def solve():
-    puzzle = open("day25/data.txt", "r").readlines()
+    puzzle = open("day25/test.txt", "r").readlines()
     result = 0
     for line in puzzle:
         line = line[:-1]
         decimal = snafu_to_decimal(line)
         result += decimal
         # snafu = decimal_to_snafu(decimal)
-        # print(line + " --> " + str(decimal) + " --> " + str("".join(snafu)))
+        # base_5 = decimal_to_base_5(decimal)
+        # base_5.reverse()
+        # print(line + " --> " + str(decimal) + " --> " + str(base_5) + " --> " + str("".join(snafu)))
     snafu = decimal_to_snafu(result)
     print(result)
     print("".join(snafu))
